edbf1fa8e68804294b67c06e77064e7eb2be96c9
Antonio Carlos <baptistaacbc@hotmail.com>
2017-11-21 12:41:32 -0200

    Adicionada classe abelha e cidade ja adicionando número aleatório de HQs + número de abelhas

--- simulacao_policia.py
+++ simulacao_policia.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import math, random, copy
 
+'''
+*	Class Caminho:
+*	Responsável por criar um caminho entre o ponto A e o ponto B (também classes)
+'''
 class Caminho:
 	def __init__(self, pontoA, pontoB):
 		self.pontoA = pontoA
@@ -8,9 +12,19 @@ class Caminho:
 		self.distancia = 0
 		self.feromonio = (random.randint(5,9)/10000.0) + (random.randint(1,9)/100000.0)
 		# self.feromonio = 400	
+		self.taxa_decaimento = 1
 
 	def setDistancia(self, distancia):
 		self.distancia = distancia
+
+	def setFeromonio(self, novoFeromonio):
+		self.feromonio = novoFeromonio
+
+	def setTaxaDecaimento(self, nova_taxa):
+		self.taxa_decaimento = nova_taxa
+
+	def decaiFeromonio(self, decaimento):
+		self.feromonio = self.feromonio/self.taxa_decaimento
 
 class Ponto:
 	def __init__(self, x, y):
@@ -57,7 +71,8 @@ class Ponto:
 				self.caminho_escolhido = (probabilidade/somatoria_probabilidades)
 				self.index = index
 		
-		return self.index, self.getCaminhos()[self.index], self.caminho_escolhido
+		# return self.index, self.getCaminhos()[self.index], self.caminho_escolhido
+		return self.index, self.caminho_escolhido
 
 	def setObjeto(self, status):
 		self.objeto = status
@@ -66,11 +81,14 @@ class Ponto:
 		return self.objeto
 
 class Cidade:
-	def __init__(self):
+	def __init__(self, iteracoes):
 		self.numero_pontos = random.randint(5, 10)
 		self.pontos = []
+		self.iteracoes = iteracoes
 		self.formigas = []
 		self.caminhos = []
+		self.mediaAbelhas = random.randint(2,4)
+		self.hqs = []
 		print('QTD PONTOS: ', self.numero_pontos)
 		for ponto in range(self.numero_pontos):
 			a = Ponto(random.randint(1,10), random.randint(1,10))
@@ -100,17 +118,38 @@ class Cidade:
 
 	def criaFormigas(self):
 		self.qtdFormigas = int(round((float) (self.numero_pontos * 1)/3))
-		# print('Qtd de Formigas: ', self.qtdFormigas)
+		print('QTD DE FORMIGAS: ', self.qtdFormigas)
 		for formiga in range(self.qtdFormigas):
 			self.formigas.append(Formiga('Patrulha', self.getPontoAleatorio()))
+		# for formiga in self.formigas:
+		# 	print(formiga.pontoAtual.nome)
+
+	def criaHQs(self):
+		self.qtdHQs = int(round((float) (self.numero_pontos * 1)/5))
+		print('QTD DE HQs: ', self.qtdHQs)
+		print('QTD DE Abelhas: ', self.mediaAbelhas)
+		for hq in range(self.qtdHQs):
+			self.hqs.append(HQ(self.mediaAbelhas, self.getPontoAleatorio()))
+		for hq in self.hqs:
+			print(hq.pontoAtual.nome)
+			for abelha in hq.abelhas:
+				print('Bee Name: ' + abelha.nome + 'HQ Bee Length: ' + str(len(hq.abelhas)))
 
 	def getPontoAleatorio(self):
 		while(True):
-			pontoAleatorio = random.randint(0	, self.numero_pontos-1)
-			# print(pontoAleatorio)
-			# print(self.pontos[pontoAleatorio].getObjeto())
+			pontoAleatorio = random.randint(0, self.numero_pontos-1)
 			if self.pontos[pontoAleatorio].getObjeto() == False:
+				print(self.pontos[pontoAleatorio].nome)
 				return self.pontos[pontoAleatorio]
+
+	def iniciaCidade(self):
+		iterador = 0
+		while(iterador < self.iteracoes):
+			# print("-- Iteracao ",iterador)
+			for formiga in self.formigas:
+				# formiga.caminhaAleatoriamente()
+				pass
+			iterador = iterador + 1
 
 
 
@@ -119,9 +158,52 @@ class Formiga:
 		self.funcao = funcao
 		self.pontoAtual = pontoAtual
 		self.pontoAtual.setObjeto(True)
+		self.setNome()
+
+	def setNome(self):
+		letras = ['Corine', 'Nina', 'Laraine', 'Gaspard', 'Bartolemo', 'Rebe', 'Gasper', 'Pauli', 'Sheila-kathryn', 'Gus', 'Christabella', 'Thalia', 'Tyrone', 'Dennison', 'Udale', 'Annaliese', 'Rufus', 'Zebedee', 'Philbert', 'Collin', 'Colver', 'Marcile', 'Cherie', 'Janene', 'Ainslie', 'Bernardina', 'Ursula', 'Alene', 'Horatio', 'Edita', 'Sidnee', 'Gianna', 'Ashton', 'Cymbre', 'Adda', 'Charlena', 'Karly', 'York', 'Shanna', 'Tracie', 'Brook', 'Hilario', 'Darcy', 'Lisette', 'Jakie', 'Teodoro', 'Rochell', 'Jenn', 'Annadiana', 'Clint', 'Wilbur', 'Cariotta', 'Kinnie', 'Diarmid', 'Jocko', 'Mortie', 'Jarib', 'Westleigh', 'Mair', 'Trumaine', 'Emlyn', 'Abagael', 'Em', 'Dolores', 'Erina', 'Lou', 'Golda', 'Herold', 'Bryn', 'Christiane', 'Oralia', 'Bella', 'Kathi', 'Kerry', 'Lindsay', 'Claudetta', 'Manny', 'Cosette', 'Gordy', 'Jordan', 'Dean', 'Elaine', 'Andrey', 'Solly', 'Renie', 'Pepito', 'Godfree', 'Sabina', 'Liana', 'Stevana', 'Onfre', 'Hubert', 'Leslie', 'Chev', 'Caryn', 'Rollins', 'Adriane', 'Bealle', 'Catharine', 'Ulrich', 'Ikey']
+		letra = random.randint(0, len(letras)-1)
+		numero = random.randint(1,100)
+		self.nome = str(letras[letra]) + str(letra)
 
 	def caminhaAleatoriamente(self):
+		print('Formiguinha ' + self.nome)
 		print(self.pontoAtual.getProbabilidade())
+
+class Abelha:
+	def __init__(self, funcao, id_hq, pontoAtual):
+		self.funcao = funcao
+		self.id_hq = id_hq
+		self.setNome()
+		self.pontoAtual = pontoAtual
+		self.pontoAtual.setObjeto(True)
+
+	def setNome(self):
+		letras = ['Corine', 'Nina', 'Laraine', 'Gaspard', 'Bartolemo', 'Rebe', 'Gasper', 'Pauli', 'Sheila-kathryn', 'Gus', 'Christabella', 'Thalia', 'Tyrone', 'Dennison', 'Udale', 'Annaliese', 'Rufus', 'Zebedee', 'Philbert', 'Collin', 'Colver', 'Marcile', 'Cherie', 'Janene', 'Ainslie', 'Bernardina', 'Ursula', 'Alene', 'Horatio', 'Edita', 'Sidnee', 'Gianna', 'Ashton', 'Cymbre', 'Adda', 'Charlena', 'Karly', 'York', 'Shanna', 'Tracie', 'Brook', 'Hilario', 'Darcy', 'Lisette', 'Jakie', 'Teodoro', 'Rochell', 'Jenn', 'Annadiana', 'Clint', 'Wilbur', 'Cariotta', 'Kinnie', 'Diarmid', 'Jocko', 'Mortie', 'Jarib', 'Westleigh', 'Mair', 'Trumaine', 'Emlyn', 'Abagael', 'Em', 'Dolores', 'Erina', 'Lou', 'Golda', 'Herold', 'Bryn', 'Christiane', 'Oralia', 'Bella', 'Kathi', 'Kerry', 'Lindsay', 'Claudetta', 'Manny', 'Cosette', 'Gordy', 'Jordan', 'Dean', 'Elaine', 'Andrey', 'Solly', 'Renie', 'Pepito', 'Godfree', 'Sabina', 'Liana', 'Stevana', 'Onfre', 'Hubert', 'Leslie', 'Chev', 'Caryn', 'Rollins', 'Adriane', 'Bealle', 'Catharine', 'Ulrich', 'Ikey']
+		letra = random.randint(0, len(letras)-1)
+		numero = random.randint(1,100)
+		self.nome = str(letras[letra]) + str(letra)
+
+class HQ:
+	def __init__(self, numero_abelhas, pontoAtual):
+		self.numero_abelhas = numero_abelhas
+		self.setNome()
+		self.pontoAtual = pontoAtual
+		self.pontoAtual.setObjeto(True)
+		self.abelhas = []
+		self.criaAbelhas()
+
+	def criaAbelhas(self):
+		for iterador in range(0, self.numero_abelhas):
+			abelhaTemp = Abelha('Tropa', self.nome, self.pontoAtual)
+			self.abelhas.append(abelhaTemp)
+
+	def setNome(self):
+		letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+		letra = random.randint(0, len(letras)-1)
+		numero = random.randint(1,100)
+		self.nome =  str(letra) + str(letras[letra])
+
 
 #-------------------- MAIN --------------------#
 if __name__ == "__main__":
@@ -140,6 +222,8 @@ if __name__ == "__main__":
 	form1.caminhaAleatoriamente()
 	'''
 
-	cidade1 = Cidade()
+	cidade1 = Cidade(30)
 	cidade1.criaFormigas()
+	cidade1.criaHQs()
+	cidade1.iniciaCidade()
 	# cidade1.mostraCidade()
