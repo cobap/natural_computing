60585adc5eef2dd855b3a0838037a9b0fa87b04c
Antonio Carlos <baptistaacbc@hotmail.com>
2017-12-21 00:17:15 -0200

    Added alllll

--- otimiza_frota.py
+++ otimiza_frota.py
@@ -2,6 +2,7 @@
 import math, random, copy
 import networkx as nx
 import matplotlib.pyplot as plt
+from random import choice
 
 '''
 *	Class Ponto:
@@ -10,11 +11,12 @@ import matplotlib.pyplot as plt
 *	É responsável por identificar se existe alguma outra classe utilizando de sua posição
 '''
 class Ponto:
-	def __init__(self, x, y):
+	def __init__(self, x, y, numero):
 		self.x = x
 		self.y = y
 		self.objeto = False
 		self.nome = self.setNome()
+		self.numero = numero
 
 	def setNome(self):
 		letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -55,16 +57,137 @@ class Caminho:
 	def __str__(self):
 		return('CAMINHO ' + self.pontoA.nome + ' - ' + self.pontoB.nome)
 
+class Formiga:
+	def __init__(self, funcao, pontoAtual):
+		self.funcao = funcao
+		# self.forca = forca
+		self.pontoAtual = pontoAtual
+		# Define que o ponto atual da formiga agora esta ocupado
+		# self.pontoAtual.setObjeto(True)
+		self.pontoAtual.setObjeto(True)
+		# Cria um nome para a formiga
+		self.setNome()
+
+	def setNome(self):
+		letras = ['Corine', 'Nina', 'Laraine', 'Gaspard', 'Bartolemo', 'Rebe', 'Gasper', 'Pauli', 'Sheila-kathryn', 'Gus', 'Christabella', 'Thalia', 'Tyrone', 'Dennison', 'Udale', 'Annaliese', 'Rufus', 'Zebedee', 'Philbert', 'Collin', 'Colver', 'Marcile', 'Cherie', 'Janene', 'Ainslie', 'Bernardina', 'Ursula', 'Alene', 'Horatio', 'Edita', 'Sidnee', 'Gianna', 'Ashton', 'Cymbre', 'Adda', 'Charlena', 'Karly', 'York', 'Shanna', 'Tracie', 'Brook', 'Hilario', 'Darcy', 'Lisette', 'Jakie', 'Teodoro', 'Rochell', 'Jenn', 'Annadiana', 'Clint', 'Wilbur', 'Cariotta', 'Kinnie', 'Diarmid', 'Jocko', 'Mortie', 'Jarib', 'Westleigh', 'Mair', 'Trumaine', 'Emlyn', 'Abagael', 'Em', 'Dolores', 'Erina', 'Lou', 'Golda', 'Herold', 'Bryn', 'Christiane', 'Oralia', 'Bella', 'Kathi', 'Kerry', 'Lindsay', 'Claudetta', 'Manny', 'Cosette', 'Gordy', 'Jordan', 'Dean', 'Elaine', 'Andrey', 'Solly', 'Renie', 'Pepito', 'Godfree', 'Sabina', 'Liana', 'Stevana', 'Onfre', 'Hubert', 'Leslie', 'Chev', 'Caryn', 'Rollins', 'Adriane', 'Bealle', 'Catharine', 'Ulrich', 'Ikey']
+		letra = random.randint(0, len(letras)-1)
+		numero = random.randint(1,100)
+		self.nome = 'ANT-' + str(letras[letra]) + str(letra)
+
+	def caminhaACO(self, caminhos, alfa, beta):
+		# print('FORMIGA '+self.nome + ' |	LUGAR '+self.pontoAtual.nome)
+		caminhos = [caminho for caminho in caminhos if caminho.pontoA.nome == self.pontoAtual.nome or caminho.pontoB.nome == self.pontoAtual.nome]
+		# print('AFTER-----------------------------------')
+		#ENTENDER PQ PRECISA DISSO
+		for caminho in caminhos:
+			if caminho.feromonio == 0:
+				caminho.feromonio = 0.0001
+			# print("  ", str(caminho))
+
+		# Vetor que será armazenada as probabilidades dos caminhos
+		probabilidades = []
+		for caminho in caminhos:
+			# Calcula a probabilidade baseado no feromonio do caminho e a sua distancia
+			probabilidade = (math.pow(caminho.feromonio, alfa) * math.pow(1.0 / caminho.distancia, beta))
+			# print('FERO, ALFA, DIST, BETA', caminho.feromonio, alfa, 1.0/caminho.distancia, beta)
+			# print('PROB', probabilidade)
+			probabilidades.append(probabilidade)
+		# Somatório de todas as probabilidades
+		somatoria_probabilidades = sum(float(prob) for prob in probabilidades)
+		
+		caminho_escolhido = 0
+		index_escolhido = -1
+		#Para todos as probabilidades calculadas
+		for index, probabilidade in zip(range(len(probabilidades)), probabilidades):
+			# print('Caminho', str(caminhos[index]) ,'Probabilidade: ', probabilidade/somatoria_probabilidades, 'Distancia', caminhos[index].distancia)
+			# Selecionada caminho escolhido como maior caso sua probabilidade seja a maior de todas
+			# print('############################')
+			# print(probabilidade)
+			# print(somatoria_probabilidades)
+			if((probabilidade/somatoria_probabilidades) > caminho_escolhido):
+				# Recalcula o caminho escolhido
+				caminho_escolhido = (probabilidade/somatoria_probabilidades)
+				# Define o index
+				# print('MELHRO INDEX É', index)
+				index_escolhido = index
+			# print('############################')
+
+		# print('MELHOR PONTO - INDEX', index_escolhido)
+		# print('Caminho', str(caminhos[index_escolhido]) ,'Probabilidade: ', probabilidades[index_escolhido]/somatoria_probabilidades, 'Distancia', caminhos[index_escolhido].distancia)
+		
+		if(index_escolhido == -1):
+			# print('caminhando aleatóriamente')
+			return self.caminhaAleatoriamente(caminhos)
+
+		# Sai do vertice atual
+		self.pontoAtual.setObjeto(False)
+		# Define o vertice atual como o novo vertice - de onde foi do ponto A para o ponto B
+		if(self.pontoAtual.nome == str(caminhos[index_escolhido])):
+			self.pontoAtual = caminhos[index_escolhido].pontoB
+		else:
+			self.pontoAtual = caminhos[index_escolhido].pontoA
+		# Define que esta ocupando o ponto B agora
+		self.pontoAtual.setObjeto(True)
+		# Define novo feromonio (por enquanto aleatóriamente)
+		# print(str(caminhos[index_escolhido]))
+		# print('FEROMONIO ANTIGO: '+ str(caminhos[index_escolhido].feromonio))
+		# caminhos[index_escolhido].setFeromonio((random.randint(5,9)/10000.0) + (random.randint(1,9)/100000.0))
+		caminhos[index_escolhido].aumentaFeromonio()
+		# print('FEROMONIO NOVO: '+ str(caminhos[index_escolhido].feromonio))
+
+		# return index, self.getCaminhos()[index], caminho_escolhido
+		return index, caminho_escolhido
+
+def caminhaAleatoriamente(formiga, grafo):
+		print(formiga.pontoAtual.numero)
+
+		# pontoAntigo = formiga.pontoAtual
+		# caminhos2 = g.neighbors(pontoAtual)
+		# caminhoEscolhido2 = random.choice(caminhos2)
+		# formiga.pontoAtual.setObjeto(False)
+		# formiga.pontoAtual = g.node[no]['ponto']
+		# formiga.pontoAtual.setObjeto(True)
+		# g[pontoAntigo][pontoAtual]['caminho'].setFeromonio((random.randint(5,9)/10000.0) + (random.randint(1,9)/100000.0))
+
 #-------------------- MAIN --------------------#
 if __name__ == "__main__":
 	g = nx.fast_gnp_random_graph(5, 0.4)
+	while(nx.is_connected(g) is not True):
+		g = nx.fast_gnp_random_graph(5, 0.4)	
 	g.nodes(data=True)
 	for no in g.nodes():
-		ponto = Ponto(random.randint(1,10), random.randint(1,10))
+		ponto = Ponto(random.randint(1,10), random.randint(1,10), no)
 		g.node[no]['ponto'] = ponto
-		# print(g.node[no]['ponto'])
-	for aresta in g.edges():
-		print(aresta[0], aresta[1])
+		g.node[no]['formiga'] = None
+		print(no, g.node[no]['ponto'].nome)
+		# print(g.node[aresta[0]]['ponto'])
+	for aresta in g.edges(data=True):
+		caminho = Caminho(g.node[aresta[0]]['ponto'], g.node[aresta[1]]['ponto'])
+		print(caminho)
+		g[aresta[0]][aresta[1]]['caminho'] = caminho
+		# print(g[aresta[0]][aresta[1]]['caminho'].feromonio)
+	
+	# no_escolhido = g.node(random.randint(1,5))
+	# print(no_escolhido)
+
+	no_ale = random.randint(1,4)
+	formiga1 = Formiga('PATRULHA', g.node[no_ale]['ponto'])
+	print('FORMIGA 1', formiga1.pontoAtual)
+	g.node[no_ale]['formiga'] = formiga1
+	
+	for no in g.nodes():
+		if(g.node[no]['formiga'] is not None):
+			caminhaAleatoriamente(g.node[no]['formiga'], g)
+
+	# print(g.node[no_ale]['formiga'])
+
+	# Mostra todas as Formigas no mapa
+	# for no in g.nodes():
+		# print(g.node[no]['formiga'])
+
+	# Mostra todo os vizinhos de um nó aleatório
+	# print(no_ale, g.neighbors(no_ale))
+
 	nx.draw(g, with_labels=True)
 	# pos=nx.spring_layout(g)
 	# nx.draw_networkx_labels(g, pos, font_size=20,font_family='sans-serif')
