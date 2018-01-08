db36647a84aad197afaf921e937833444b4d92d1
Antonio Carlos <baptistaacbc@hotmail.com>
2018-01-08 19:18:49 -0200

    Added perfect csvs

--- clean_files.py
+++ clean_files.py
@@ -7,5 +7,5 @@ arquivo = arquivo.replace('\n', '')
 with open("./results/" + arquivo + ".csv", "r") as fin:
     with open("./results/" + arquivo + "_limpo.csv", "w") as fout:
         for line in fin:
-        	fout.write(line.replace('(', '').replace(")", "").replace("\'", "\""))
-os.remove("./results/" + arquivo + ".csv")
+        	fout.write(line.replace('(', '').replace(")", "").replace("\'", "\"").replace('"ATIVO"', "0").replace('"COMPLETO"', "1").replace('"ON-HOLD"', "2").replace('"RE-ATIVO"', "3").replace('"TIME-OUT"', "4"))
+# os.remove("./results/" + arquivo + ".csv")
--- plot_graphs.py
+++ plot_graphs.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
+import sys
 import numpy as np
 
-data = np.genfromtxt('./results/eventos_output1_limpo.csv', delimiter=',', skip_header=10, skip_footer=10, names=['EVENTO', 'NOME', 'INTENSIDADE', 'PONTO', 'GRAU_ATUAL', 'GRAU_ORIGINAL', 'RODADAS', 'STATUS'])
+data = np.genfromtxt('./results/eventos_output' + sys.argv[1] + '_limpo.csv', delimiter=',', skip_header=10, skip_footer=10, names=['EVENTO', 'NOME', 'INTENSIDADE', 'PONTO', 'GRAU_ATUAL', 'GRAU_ORIGINAL', 'RODADAS', 'STATUS'])
 
-plt.plot(data['PONTO'], data['INTENSIDADE'], label='linear')
+# plt.plot(data['PONTO'], data['INTENSIDADE'], label='linear')
+# plt.hist(data['PONTO'], normed=True, bins=30)
+# alturas = list(range(647))
+# plt.bar(data['PONTO'], height=alturas)
 plt.legend()
 plt.show()
